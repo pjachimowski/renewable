82e2bb83faa1a94e0206603a513a7f6abfe0cba7
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2019-09-25 12:16:53 +0000

    closing brackets issue

--- static/js/graphs2.js
+++ static/js/graphs2.js
@@ -15,7 +15,7 @@ function makeGraphs(error, statesData) {
     show_council_votes(ndx);
     show_parliament_seats(ndx);
 
-    show_gdp_per_country(ndx);
+    show_gdp_per_country();
 
 
     dc.renderAll();
@@ -118,7 +118,7 @@ function show_parliament_seats(ndx) {
 
 /* ----------------- show_gdp_per_country -----------------------*/
 
-function show_gdp_per_country(ndx) {
+function show_gdp_per_country() {
     // append the svg object to the body of the page
     var svg = d3.select("#gdp-per-country")
         .append("svg")
@@ -129,101 +129,101 @@ function show_gdp_per_country(ndx) {
     var height = 460
 
     // Read data
-     // d3.csv("https://github.com/pjachimowski/renewable/blob/master/data/states.csv", function(ndx) {
-
-        // Filter a bit the data -> more than 1 million inhabitants
-        var data = data.filter(function(d) { return d.GDP_eur > 9000 })
-
-        // Color palette for continents?
-        var color = d3.scale.ordinal()
-            .domain(["Candidate", "Member", "Non member"])
-            .range(d3.schemeSet1);
-
-        // Size scale for countries
-        var size = d3.scale.linear()
-            .domain([0, 1400000000])
-            .range([7, 55]) // circle will be between 7 and 55 px wide
-
-        // create a tooltip
-        var Tooltip = d3.select("#gdp-per-country")
-            .append("div")
-            .style("opacity", 0)
-            .attr("class", "tooltip")
-            .style("background-color", "white")
-            .style("border", "solid")
-            .style("border-width", "2px")
-            .style("border-radius", "5px")
-            .style("padding", "5px")
-
-        // Three function that change the tooltip when user hover / move / leave a cell
-        var mouseover = function(d) {
-            Tooltip
-                .style("opacity", 1)
-        }
-        var mousemove = function(d) {
-            Tooltip
-                .html('<u>' + d.key + '</u>' + "<br>" + d.GDP_eur + " GDP")
-                .style("left", (d3.mouse(this)[0] + 20) + "px")
-                .style("top", (d3.mouse(this)[1]) + "px")
-        }
-        var mouseleave = function(d) {
-            Tooltip
-                .style("opacity", 0)
-        }
-
-        // Initialize the circle: all located at the center of the svg area
-        var node = svg.append("g")
-            .selectAll("circle")
-            .ndx(ndx)
-            .enter()
-            .append("circle")
-            .attr("class", "node")
-            .attr("r", function(d) { return size(d.GDP_eur) })
-            .attr("cx", width / 2)
-            .attr("cy", height / 2)
-            .style("fill", function(d) { return color(d.European_Union) })
-            .style("fill-opacity", 0.8)
-            .attr("stroke", "black")
-            .style("stroke-width", 1)
-            .on("mouseover", mouseover) // What to do when hovered
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave)
-            .call(d3.drag() // call specific function when circle is dragged
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended));
-
-        // Features of the forces applied to the nodes:
-        var simulation = d3.forceSimulation()
-            .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-            .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-            .force("collide", d3.forceCollide().strength(.2).radius(function(d) { return (size(d.GDP_eur) + 3) }).iterations(1)) // Force that avoids circle overlapping
-
-        // Apply these forces to the nodes and update their positions.
-        // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-        simulation
-            .nodes(data)
-            .on("tick", function(d) {
-                node
-                    .attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; })
-            });
-
-        // What happens when a circle is dragged?
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(.03).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
-
-        function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(.03);
-            d.fx = null;
-            d.fy = null;
-        }
+    d3.csv("https://raw.githubusercontent.com/pjachimowski/renewable/master/data/states.csv", function(data) {
     })
+            // Filter a bit the data -> more than 1 million inhabitants
+            //data = data.filter(function(d) { return d.GDP_eur > 9000 })
+            var data = data.filter(function(d) { return d.GDP_eur > 9000 });
+
+            // Color palette for continents?
+            var color = d3.scale.ordinal()
+                .domain(["Candidate", "Member", "Non member"])
+                .range(d3.schemeSet1);
+
+            // Size scale for countries
+            var size = d3.scale.linear()
+                .domain([0, 1400000000])
+                .range([7, 55]) // circle will be between 7 and 55 px wide
+
+            // create a tooltip
+            var Tooltip = d3.select("#gdp-per-country")
+                .append("div")
+                .style("opacity", 0)
+                .attr("class", "tooltip")
+                .style("background-color", "white")
+                .style("border", "solid")
+                .style("border-width", "2px")
+                .style("border-radius", "5px")
+                .style("padding", "5px")
+
+            // Three function that change the tooltip when user hover / move / leave a cell
+            var mouseover = function(d) {
+                Tooltip
+                    .style("opacity", 1)
+            }
+            var mousemove = function(d) {
+                Tooltip
+                    .html('<u>' + d.key + '</u>' + "<br>" + d.GDP_eur + " GDP")
+                    .style("left", (d3.mouse(this)[0] + 20) + "px")
+                    .style("top", (d3.mouse(this)[1]) + "px")
+            }
+            var mouseleave = function(d) {
+                Tooltip
+                    .style("opacity", 0)
+            }
+
+            // Initialize the circle: all located at the center of the svg area
+            var node = svg.append("g")
+                .selectAll("circle")
+                .ndx(ndx)
+                .enter()
+                .append("circle")
+                .attr("class", "node")
+                .attr("r", function(d) { return size(d.GDP_eur) })
+                .attr("cx", width / 2)
+                .attr("cy", height / 2)
+                .style("fill", function(d) { return color(d.European_Union) })
+                .style("fill-opacity", 0.8)
+                .attr("stroke", "black")
+                .style("stroke-width", 1)
+                .on("mouseover", mouseover) // What to do when hovered
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave)
+                .call(d3.drag() // call specific function when circle is dragged
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended));
+
+            // Features of the forces applied to the nodes:
+            var simulation = d3.forceSimulation()
+                .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+                .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+                .force("collide", d3.forceCollide().strength(.2).radius(function(d) { return (size(d.GDP_eur) + 3) }).iterations(1)) // Force that avoids circle overlapping
+
+            // Apply these forces to the nodes and update their positions.
+            // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+            simulation
+                .nodes(data)
+                .on("tick", function(d) {
+                    node
+                        .attr("cx", function(d) { return d.x; })
+                        .attr("cy", function(d) { return d.y; })
+                });
+
+            // What happens when a circle is dragged?
+            function dragstarted(d) {
+                if (!d3.event.active) simulation.alphaTarget(.03).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+
+            function dragended(d) {
+                if (!d3.event.active) simulation.alphaTarget(.03);
+                d.fx = null;
+                d.fy = null;
+            }
